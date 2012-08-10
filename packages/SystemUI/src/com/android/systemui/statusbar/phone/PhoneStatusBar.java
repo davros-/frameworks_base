2c1fe25bf91f939f91cda2fd2bd948583f2f60dc
Danesh Mondegarian <daneshm90@gmail.com>
2012-08-10 14:56:11 -0400

    NavigationBar : Customization
    Includes:
    
    - Allows user to customize navigation bar.
      Features :
       - Choose from 1-6 # of buttons
       - Re-arrange / assign buttons
      Id: Idc9f5f401f15d207504e49de47666bb5510a06ca
    
    - Navbar : Rework logic for determining orientation
      This patch introduces better checks for hybrid ui's.
      This should fix issues of small buttons due to incorrect ui determination
      Thanks Rolf Arne Schulze for testing :)
      Id: If5b61dc4764a98deedb5b801d11451fed2488cba
    
    Change-Id: I8903b85b6ee8581f8564d9dc7b0d40f645d7647a
    
    Conflicts:
    
    	packages/SystemUI/res/layout/navigation_bar.xml
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -785,11 +785,7 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
-
-        mNavigationBarView.getRecentsButton().setOnClickListener(mRecentsClickListener);
-        mNavigationBarView.getRecentsButton().setOnTouchListener(mRecentsPreloadOnTouchListener);
-        mNavigationBarView.getHomeButton().setOnTouchListener(mHomeSearchActionListener);
-        mNavigationBarView.getSearchLight().setOnTouchListener(mHomeSearchActionListener);
+        mNavigationBarView.setListener(mRecentsClickListener,mRecentsPreloadOnTouchListener, mHomeSearchActionListener);
         updateSearchPanel();
     }
 
@@ -2389,6 +2385,7 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         makeStatusBarView();
         repositionNavigationBar();
+        mNavigationBarView.updateResources();
 
         // recreate StatusBarIconViews.
         for (int i = 0; i < nIcons; i++) {
@@ -2540,7 +2537,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     @Override
     protected boolean shouldDisableNavbarGestures() {
         return !isDeviceProvisioned()
-                || mExpandedVisible
+                || mExpandedVisible || NavigationBarView.getEditMode()
                 || (mDisabled & StatusBarManager.DISABLE_SEARCH) != 0;
     }
 
