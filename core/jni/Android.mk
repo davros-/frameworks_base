68e4d8de77dbac5bbe52bd42eda915c90888f2c8
Lucas Crowthers <lucasc@codeaurora.org>
2013-01-08 15:54:49 -0500

    frameworks/base: vectorize Bitmap operations
    Enable gcc vectorizing optimizations for Bitmap operations within
    libandroid_runtime.so.  Adds -ftree-vectorize -mvectorize-with-neon-quad
    -fprefetch-loop-arrays to functions and compiles Bitmap.cpp in ARM mode
    (necessary to enable vectorizing optimizations) for appropriate targets.
    
    Change-Id: I12defef6f47adf41af849df6e750097f5906d92d
    
    Conflicts:
    
    	core/jni/Android.mk

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -88,7 +88,6 @@ LOCAL_SRC_FILES:= \
 	android_util_XmlBlock.cpp \
 	android_util_PackageRedirectionMap.cpp \
 	android/graphics/AutoDecodeCancel.cpp \
-	android/graphics/Bitmap.cpp \
 	android/graphics/BitmapFactory.cpp \
 	android/graphics/Camera.cpp \
 	android/graphics/Canvas.cpp \
@@ -226,8 +225,8 @@ LOCAL_SHARED_LIBRARIES += libselinux
 LOCAL_CFLAGS += -DHAVE_SELINUX
 endif # HAVE_SELINUX
 
-ifeq ($(TARGET_ARCH), arm)
   ifeq ($(ARCH_ARM_HAVE_NEON), true)
+  ifeq ($(TARGET_USE_KRAIT_BIONIC_OPTIMIZATION), true)
     TARGET_arm_CFLAGS += -DUSE_NEON_BITMAP_OPTS -mvectorize-with-neon-quad
     LOCAL_SRC_FILES+= \
 		android/graphics/Bitmap.cpp.arm
