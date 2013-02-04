1ec268e7378a93f745b168fa10848f9592e1ee6b
Lars Greiss <kufikugel@googlemail.com>
2013-02-04 13:51:20 +0100

    Frameworks: Expanded desktop mode (1/2)
    this commit adds expanded desktop mode. You can decide if only
    navigation bar, only status bar or both are hidden when expanded
    desktop was activated.
    
    This commit also sorts the entries a bit more logical and alphabetical
    
    Change-Id: I3fe0b1eee6e13cf10debbcbe5338fa901b785c84
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2863,10 +2863,17 @@ public final class Settings {
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
          /**
-         * Statusbar State
+         * Whether Expanded desktop is currently running or not
          * @hide
          */
         public static final String EXPANDED_DESKTOP_STATE = "expanded_desktop_state";
+
+         /**
+         * Expanded desktop mode
+         * 0 = none, 1 = hide only navbar, 2 = hide only statusbar, 3 = hide both
+         * @hide
+         */
+        public static final String EXPANDED_DESKTOP_MODE = "expanded_desktop_mode";
 
         /**
          * Whether to use a separate delay for "slide to unlock" and security
@@ -2964,7 +2971,7 @@ public final class Settings {
         public static final String QUIET_HOURS_MUTE = "quiet_hours_mute";
 
         /**
-         * Whether to disable haptic feedback during quiet hours.
+         * Whether to disable haptic feedback during quiet hours.)
          * @hide
          */
         public static final String QUIET_HOURS_HAPTIC = "quiet_hours_haptic";
