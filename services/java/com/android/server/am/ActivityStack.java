3d2704b7f833e7cd730b4940d131c407584a3fd7
Chris Phelps <cphelps76@cphelps76-HP-Pavilion-dv7.(none)>
2013-02-12 19:42:24 -0600

    Revert "Add CPU boosting hooks"
    This reverts commit 2d5c97de036496de852550fcf2362b42a5ca41a3.
    
    Conflicts:
    
    	services/java/com/android/server/am/ActivityStack.java

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -22,7 +22,6 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import com.android.internal.app.HeavyWeightSwitcherActivity;
 import com.android.internal.os.BatteryStatsImpl;
 import com.android.server.am.ActivityManagerService.PendingActivityLaunch;
-import com.android.server.power.PowerManagerService;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -54,7 +53,6 @@ import android.os.Message;
 import android.os.ParcelFileDescriptor;
 import android.os.PowerManager;
 import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
@@ -310,8 +308,6 @@ final class ActivityStack {
         }
     }
 
-    private final PowerManagerService mPm;
-
     private static final ActivityTrigger mActivityTrigger;
 
     static {
@@ -450,7 +446,6 @@ final class ActivityStack {
             (PowerManager)context.getSystemService(Context.POWER_SERVICE);
         mGoingToSleep = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ActivityManager-Sleep");
         mLaunchingActivity = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ActivityManager-Launch");
-        mPm = (PowerManagerService) ServiceManager.getService("power");
         mLaunchingActivity.setReferenceCounted(false);
     }
 
@@ -1435,9 +1430,6 @@ final class ActivityStack {
     }
 
     final boolean resumeTopActivityLocked(ActivityRecord prev, Bundle options) {
-
-        mPm.cpuBoost(1500000);
-
         // Find the first activity that is not finishing.
         ActivityRecord next = topRunningActivityLocked(null);
 
@@ -2497,8 +2489,6 @@ final class ActivityStack {
             boolean componentSpecified, ActivityRecord[] outActivity) {
 
         int err = ActivityManager.START_SUCCESS;
-
-        mPm.cpuBoost(1500000);
 
         ProcessRecord callerApp = null;
         if (caller != null) {
