961e7cfb0d955dd3a24e233fa5579b705e1a9134
davros <davros@droidconcepts.com>
2013-02-23 08:04:54 -0500

    Quicksettings: fix updating 2G,LTE and WifiTether tiles adding an observer to listen for state changes
    also move updating torch and navbarhide
    
    Change-Id: I8dc8f9d804fcd47daaec50f0169fd5148d494cc6
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -282,6 +282,7 @@ public class QuickSettings {
         filter.addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
         filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
         filter.addAction(Intent.ACTION_USER_SWITCHED);
+	filter.addAction(WifiManager.WIFI_AP_STATE_CHANGED_ACTION);
         mContext.registerReceiver(mReceiver, filter);
 
         IntentFilter profileFilter = new IntentFilter();
@@ -292,6 +293,7 @@ public class QuickSettings {
 
         new SettingsObserver(new Handler()).observe();
         new SoundObserver(new Handler()).observe();
+	new NetworkModeObserver(new Handler()).observe();
     }
 
     void setBar(PanelBar bar) {
@@ -1584,6 +1586,8 @@ public class QuickSettings {
             } else if (Intent.ACTION_USER_SWITCHED.equals(action)) {
                 reloadUserInfo();
                 reloadFavContactInfo();
+	    } else if (WifiManager.WIFI_AP_STATE_CHANGED_ACTION.equals(action)) {
+		mHandler.postDelayed(delayedRefresh, 1000);
             }
         }
     };
@@ -1680,7 +1684,6 @@ public class QuickSettings {
         public void run() {
             mModel.refreshWifiTetherTile();
             mModel.refreshUSBTetherTile();
-            mModel.refreshTorchTile();
         }
     };
 
@@ -1723,6 +1726,7 @@ public class QuickSettings {
         updateWifiDisplayStatus();
         updateResources();
         reloadFavContactInfo();
+	mModel.refreshTorchTile();
     }
 
     class SettingsObserver extends ContentObserver {
@@ -1770,6 +1774,27 @@ public class QuickSettings {
             mModel.refreshVibrateTile();
             mModel.refreshSilentTile();
             mModel.refreshSoundStateTile();
+        }
+    }
+
+    class NetworkModeObserver extends ContentObserver {
+        NetworkModeObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global
+                    .getUriFor(Settings.Global.PREFERRED_NETWORK_MODE),
+                    false, this);
+            mModel.refresh2gTile();
+            mModel.refreshLTETile();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            mModel.refresh2gTile();
+            mModel.refreshLTETile();
         }
     }
 }
