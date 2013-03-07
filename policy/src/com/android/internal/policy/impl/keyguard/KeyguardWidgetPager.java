e397d1758875afa81c81583723f3aeb3b6888dca
davros <davros@droidconcepts.com>
2013-03-07 11:58:41 -0500

    forget a , get 347 errors ... good times

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
@@ -54,7 +54,7 @@ public class KeyguardWidgetPager extends PagedView implements PagedView.PageSwit
 
     private static final String[] CLOCK_WIDGET_PACKAGES = new String[] {
         "com.cyanogenmod.lockclock",
-        "com.android.deskclock"
+        "com.android.deskclock",
         "com.android.deskclock"
     };
 
