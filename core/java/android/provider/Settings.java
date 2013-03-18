328bdde246ce83b8364ce40206b35cc8c8d57ba7
davros <davros@droidconcepts.com>
2013-03-18 11:23:39 -0400

    camera widget toggle

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3490,6 +3490,11 @@ public final class Settings {
           */
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
+         /**
+          * @hide
+          */
+         public static final String KG_CAMERA_WIDGET = "kg_camera_widget";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
