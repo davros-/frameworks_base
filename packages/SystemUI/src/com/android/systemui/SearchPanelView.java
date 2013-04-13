dd0e82c3558ddbfad06a1e501bb4072f57763c73
PrimeDirective <activethrasher00@gmail.com>
2013-04-13 15:32:44 -0400

    systemui: fix moar memory leaks
    used example commits in AOKP & CM to fix memory leaks within Vanir source using
    onDetachedFromWindow to unregister content resolvers that were being left behind
    every time the view was redrawn
    
    PS2: searchpanelview: move setdrawables back to onfinishinflate
         change signalclusterview's updatesettings back to our feature
         style instead of aokp's
    
    PS3: good to go
    
    ps6: prevent double-observe and double-unobserve of SettingsObserver
    
    Change-Id: I09c15bfe78ad5edab6a279d4695153313c9dfbdf
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -113,7 +113,8 @@ public class SearchPanelView extends FrameLayout implements
 
     private PackageManager mPackageManager;
     private Resources mResources;
-    private TargetObserver mTargetObserver;
+    private SettingsObserver mObserver;
+
     private ContentResolver mContentResolver;
     private String[] targetActivities = new String[5];
     private String[] longActivities = new String[5];
@@ -143,13 +144,7 @@ public class SearchPanelView extends FrameLayout implements
         mResources = mContext.getResources();
 
         mContentResolver = mContext.getContentResolver();
-
-        mAokpTarget = new AokpTarget(context);
-
-        SettingsObserver observer = new SettingsObserver(new Handler());
-        observer.observe();
-        updateSettings();
-
+        mObserver = new SettingsObserver(new Handler());
     }
 
     private class H extends Handler {
@@ -477,6 +472,20 @@ public class SearchPanelView extends FrameLayout implements
         return true;
     }
 
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        mObserver.observe();
+        updateSettings();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mObserver.unobserve();
+    }
+
     /**
      * Whether the panel is showing, or, if it's animating, whether it will be
      * when the animation is done.
@@ -522,43 +531,41 @@ public class SearchPanelView extends FrameLayout implements
         return mResources.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
     }
 
-    public class TargetObserver extends ContentObserver {
-        public TargetObserver(Handler handler) {
-            super(handler);
-        }
-
-        @Override
-        public boolean deliverSelfNotifications() {
-            return super.deliverSelfNotifications();
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            super.onChange(selfChange);
-            setDrawables();
-            updateSettings();
-        }
-    }
-
     class SettingsObserver extends ContentObserver {
+        private ContentResolver _persistentResolver; //null if unobserved, not if observed
+
         SettingsObserver(Handler handler) {
             super(handler);
         }
 
         void observe() {
-            ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.NAVIGATION_BAR_LEFTY_MODE), false, this);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.SYSTEMUI_NAVRING_AMOUNT), false, this);
+            if (_persistentResolver == null)
+            {
+                _persistentResolver = mContext.getContentResolver();
+                _persistentResolver.registerContentObserver(Settings.System.getUriFor(
+                        Settings.System.SYSTEMUI_NAVRING_AMOUNT), false, this);
 
-            for (int i = 0; i < 5; i++) {
-	            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING[i]), false, this);
-	            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING_LONG[i]), false, this);
+                for (int i = 0; i < 5; i++) {
+	                _persistentResolver.registerContentObserver(
+                        Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING[i]), false, this);
+	                _persistentResolver.registerContentObserver(
+                        Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING_LONG[i]), false, this);
+                }
             }
+            //else BEEN THERE. DONE THAT.
+        }
 
+        void unobserve() {
+            if (_persistentResolver != null) {
+                _persistentResolver.unregisterContentObserver(this);
+                _persistentResolver = null;
+            }
+            //else BEEN THERE. DONE THAT.
+        }
+
+        @Override
+        public boolean deliverSelfNotifications() {
+            return super.deliverSelfNotifications();
         }
 
         @Override
--- packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java
@@ -96,9 +96,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
 
     int mNumberOfButtons = 3;
 
-    // Will determine if NavBar goes to the left side in Landscape Mode
-    private boolean mLeftyMode;
-
     /* 0 = Phone UI
      * 1 = Tablet UI
      * 2 = Phablet UI
@@ -182,7 +179,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
 
     public void setDelegateView(View view) {
         mDelegateHelper.setDelegateView(view);
-        //mDelegateHelper.setLefty(mLeftyMode);
     }
 
     public void setBar(BaseStatusBar phoneStatusBar) {
@@ -297,9 +293,9 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                 } else {
                         v.setImageDrawable(mAokpTarget.getIconImage(mClickActions[j]));
                 }
-                addButton(navButtonLayout, v, landscape && !mLeftyMode);
+                addButton(navButtonLayout, v, landscape);
                 // if we are in LeftyMode, then we want to add to end, like Portrait
-                addLightsOutButton(lightsOut, v, landscape && !mLeftyMode, false);
+                addLightsOutButton(lightsOut, v, landscape, false);
 
                 if (v.getId() == R.id.back){
                 	mBackIcon = mBackLandIcon = v.getDrawable();
@@ -323,12 +319,12 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                     // since we didn't add these at the beginning, we need to insert it now
                     // the behavior is backwards from landscape (ie, insert at beginning
                     // if portrait, add to end if landscape
-                    addButton(navButtonLayout, leftMenuKey, !landscape || (landscape && mLeftyMode));
-                    addLightsOutButton(lightsOut, leftMenuKey, !landscape || (landscape && mLeftyMode), true);
+                    addButton(navButtonLayout, leftMenuKey, !landscape || (landscape));
+                    addLightsOutButton(lightsOut, leftMenuKey, !landscape || (landscape), true);
 
                     View rightMenuKey = generateKey(landscape, KEY_MENU_RIGHT);
-                    addButton(navButtonLayout, rightMenuKey, landscape && !mLeftyMode);
-                    addLightsOutButton(lightsOut, rightMenuKey, landscape && !mLeftyMode, true);
+                    addButton(navButtonLayout, rightMenuKey, landscape);
+                    addLightsOutButton(lightsOut, rightMenuKey, landscape, true);
                 }
             } else {
                 // there's a big menu button.
@@ -960,8 +956,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                     Settings.System.getUriFor(Settings.System.NAVIGATION_BAR_BUTTONS_QTY), false,
                     this);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.NAVIGATION_BAR_LEFTY_MODE), false, this);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_MENU_ARROW_KEYS), false, this);
 
             for (int j = 0; j < 7; j++) { // watch all 7 settings for changes.
@@ -1026,8 +1020,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                 Settings.System.NAVIGATION_BAR_MENU_ARROW_KEYS, true);
         mCurrentUIMode = Settings.System.getInt(resolver,
                 Settings.System.CURRENT_UI_MODE,0);
-        mLeftyMode = Settings.System.getBoolean(resolver,
-                Settings.System.NAVIGATION_BAR_LEFTY_MODE, false);
         mNumberOfButtons = Settings.System.getInt(resolver,
                 Settings.System.NAVIGATION_BAR_BUTTONS_QTY, 0);
         if (mNumberOfButtons == 0) {
