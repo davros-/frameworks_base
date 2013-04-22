a8c28a6c841418138fb8b24b0b3a63090d2c3b98
Stevespear426 <stephen.k.spear@gmail.com>
2013-04-22 17:37:27 +0200

    FX: NavRing fix longpress derp.
    Change-Id: Icebb34612b65ff30e427f237780b366f94a9b24e
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -170,12 +170,11 @@ public class SearchPanelView extends FrameLayout implements
        final Runnable SetLongPress = new Runnable () {
             public void run() {
                 if (!mSearchPanelLock) {
+                    mSearchPanelLock = true;
                     mLongPress = true;
-                    Log.d(TAG,"LongPress!");
-                    mBar.hideSearchPanel();
                     mAokpTarget.launchAction(longList.get(mTarget));
                     AwesomeAction.launchAction(mContext, longList.get(mTarget));
-                    mSearchPanelLock = true;
+                    mBar.hideSearchPanel();
                  }
             }
         };
