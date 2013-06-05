4ade5020621c44806be6734906740eca1b05125d
davros <davros@droidconcepts.com>
2013-06-05 15:34:12 -0400

    seems drastict lol

--- packages/SystemUI/src/com/android/systemui/statusbar/WidgetView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/WidgetView.java
@@ -1,21 +1,7 @@
-
-/*
- * Copyright 2011 AOKP
- * Copyright 2013 SlimRoms
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.systemui.statusbar;
+
+import com.android.systemui.R;
+import com.android.systemui.WidgetSelectActivity;
 
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
@@ -24,10 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.database.ContentObserver;
-import android.graphics.drawable.Drawable;
-import android.graphics.PorterDuff.Mode;
 import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.provider.Settings;
@@ -45,9 +28,6 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.android.systemui.R;
-import com.android.systemui.WidgetSelectActivity;
-
 public class WidgetView extends LinearLayout {
 
     private Context mContext;
@@ -55,7 +35,6 @@ public class WidgetView extends LinearLayout {
     public FrameLayout mPopupView;
     public WindowManager mWindowManager;
     int originalHeight = 0;
-    LinearLayout mWidgetPanel;
     TextView mWidgetLabel;
     ViewPager mWidgetPager;
     WidgetPagerAdapter mAdapter;
@@ -65,7 +44,6 @@ public class WidgetView extends LinearLayout {
     long mDowntime;
     boolean mMoving = false;
     boolean showing = false;
-    int mCurrUiInvertedMode;
 
     final static String TAG = "Widget";
 
@@ -79,11 +57,6 @@ public class WidgetView extends LinearLayout {
         filter.addAction(WidgetReceiver.ACTION_DEALLOCATE_ID);
         filter.addAction(WidgetReceiver.ACTION_TOGGLE_WIDGETS);
         filter.addAction(WidgetReceiver.ACTION_DELETE_WIDGETS);
-
-        mCurrUiInvertedMode = mContext.getResources().getConfiguration().uiInvertedMode;
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
         mContext.registerReceiver(new WidgetReceiver(), filter);
         mHandler = new Handler();
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
@@ -287,14 +260,6 @@ public class WidgetView extends LinearLayout {
                     mAdapter.mAppWidgetHost.deleteAppWidgetId(widgetIds[i]);
                 }
                 mAdapter.mAppWidgetHost.deleteHost();
-            if (Intent.ACTION_CONFIGURATION_CHANGED.equals(action)) {
-                // detect inverted ui mode change
-                int uiInvertedMode =
-                    mContext.getResources().getConfiguration().uiInvertedMode;
-                if (uiInvertedMode != mCurrUiInvertedMode) {
-                    mCurrUiInvertedMode = uiInvertedMode;
-                    createWidgetView();
-                }
             }
         }
     }
