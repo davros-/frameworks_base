4406f21682c437628a2a3c657de4444ff48db4fc
davros <davros@droidconcepts.com>
2013-06-05 14:23:54 -0400

    build fix

--- core/java/com/android/internal/util/cm/QSUtils.java
+++ core/java/com/android/internal/util/cm/QSUtils.java
@@ -53,9 +53,4 @@ public class QSUtils {
             return NfcAdapter.getDefaultAdapter(ctx) != null;
         }
 
-        public static boolean deviceSupportsLte(Context ctx) {
-            final TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
-            return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) || tm.getLteOnGsmMode() != 0;
-        }
-
 }
